d0e4f13aa05a988c89dbccc237d973b64abccaa3
Peter M Haverty <phaverty@gene.com>
2013-06-24 18:42:57 +0000

    no longer messing with options for bigmemory dimnames
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bigmemoryExtras@77871 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 .onLoad <- function(libname,pkgname) {
   options(bigmemory.typecast.warning=FALSE)
-  options(bigmemory.allow.dimnames=TRUE)
 }
 
 .onUnLoad <- function(libpath) {
   options(bigmemory.typecast.warning=NULL)
-  options(bigmemory.allow.dimnames=FALSE)
 }
