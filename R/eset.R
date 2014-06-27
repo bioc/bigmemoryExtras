fb41f0dae32bd237481b3688da631282a9748267
Peter M Haverty <phaverty@gene.com>
2014-06-27 18:52:06 +0000

    Version: 1.9.1. Deprecated Biobase stuff, updateBackingfiles added to replace updateAssayDataPaths
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/bigmemoryExtras@91953 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/eset.R
+++ R/eset.R
@@ -3,6 +3,21 @@
 ##' @importClassesFrom Biobase AssayData eSet
 NULL
 
+##' Use BigMatrix in eSet assayData
+##'
+##' The Biobase package's eSet objects hold one or more matrices within an environment stored
+##' in their assayData slot. Use of classes other than a base matrix require a method to
+##' create an annotatedDataFrame from that class.  Users are unlikely to ever call this
+##' method.
+##'
+##' @exportMethod annotatedDataFrameFrom
+##' @rdname annotatedDataFrameFrom
+##' @param object a BigMatrix
+setMethod("annotatedDataFrameFrom", signature(object="BigMatrix"),
+          function(object) {
+              .Deprecated("nothing", msg="All eSet-related content is being deprecated.")
+              return(Biobase:::annotatedDataFrameFromMatrix(object))
+          })
 
 ##' Attach on-disk matrices into assayData
 ##'
@@ -18,20 +33,20 @@ NULL
 ##' Environment type assayData objects, even "lockedEnvironment" objects, will be
 ##' updated in place (same pointer).
 ##'
-##' @usage attachAssayDataElements( aData )
 ##' @examples \dontrun{ assayData(eset) = attachAssayDataElements( assayData(eset) ) }
 ##' @param aData assayData object (environment or list style). Usually from the assayData slot of an eSet.
 ##' @return assayData matching the supplied aData argument.
 ##' Any BigMatrix objects will be attached. Re-assignment back
 ##' original eSet only necessary if using a list type assayData, which is rare.
-##' @export 
+##' @export
 attachAssayDataElements <- function(aData) {
-  for( ad.name in assayDataElementNames(aData)) {
-    if ( is( aData[[ad.name]], "BigMatrix" ) ) {
-      aData[[ad.name]]$attach()
+    .Deprecated("nothing", msg="All eSet-related content is being deprecated.")
+    for( ad.name in assayDataElementNames(aData)) {
+        if ( is( aData[[ad.name]], "BigMatrix" ) ) {
+            aData[[ad.name]]$attach()
+        }
     }
-  }
-  return(invisible(aData))
+    return(invisible(aData))
 }
 
 ##' Update directory for BigMatrix assayDataElement to new location
@@ -41,32 +56,20 @@ attachAssayDataElements <- function(aData) {
 ##'
 ##' If you have renamed specific backing files or descriptor files, you will want to update the
 ##' datapath and descpath fields of the relevant BigMatrix objects.
-##' 
+##'
 ##' @param ds eSet
 ##' @param dir character, path to directory holding all BigMatrix files
 ##' @return eSet
 ##' @examples \dontrun{ eset = updateAssayDataElementPaths(eset, "/new/path/to/bigmat/dir") }
-##' @export 
+##' @export
 ##' @author Peter M. Haverty \email{phaverty@@gene.com}
 updateAssayDataElementPaths <- function(ds, dir) {
-  for (ad.name in assayDataElementNames(ds)) {
-    if (is(assayDataElement(ds,ad.name),"BigMatrix")) {
-      ad = assayDataElement(ds,ad.name)
-      ad$backingfile = file.path( dir, basename(assayDataElement(ds,ad.name)$backingfile))
+    .Deprecated("updateBackingfiles", "eSet-specific features are now deprecated.")
+    for (ad.name in assayDataElementNames(ds)) {
+        if (is(assayDataElement(ds,ad.name),"BigMatrix")) {
+            ad = assayDataElement(ds,ad.name)
+            ad$backingfile = file.path( dir, basename(assayDataElement(ds,ad.name)$backingfile))
+        }
     }
-  }
-  return(invisible(ds))
+    return(invisible(ds))
 }
-
-##' Use BigMatrix in eSet assayData
-##'
-##' The Biobase package's eSet objects hold one or more matrices within an environment stored
-##' in their assayData slot. Use of classes other than a base matrix require a method to
-##' create an annotatedDataFrame from that class.  Users are unlikely to ever call this
-##' method.
-##'
-##' @exportMethod annotatedDataFrameFrom
-##' @rdname annotatedDataFrameFrom
-setMethod("annotatedDataFrameFrom",
-          signature(object="BigMatrix"), Biobase:::annotatedDataFrameFromMatrix
-          )
